7b9abf62cf7c546ecdd1658b43e40842e61457c5
cptnfren <cptnfren@users.noreply.github.com>
2026-02-27 19:26:55 -0600

    chore: bump version to 1.4.0

--- bbackup/__init__.py
+++ bbackup/__init__.py
@@ -3,5 +3,5 @@ bbackup - Docker Backup Tool with Rich TUI
 A comprehensive backup solution for Docker containers, volumes, and configurations.
 """
 
-__version__ = "1.3.3"
+__version__ = "1.4.0"
 __author__ = "Slavic Kozyuk / Crux Experts LLC"
